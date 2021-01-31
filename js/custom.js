e59cb28b037cb435c5a9cbe12b315c872d8dae33
salman saad <websalmansaad@users.noreply.github.com>
2021-02-01 04:19:01 +0600

    ssd

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// slick slider js start
+$('.testimonailCarousel').slick({
+    dots: true,
+    // infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+// slick slider js end
